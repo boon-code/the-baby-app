c30ea8d20b47c26060a178d9c76ce12cbedf8d40
Manuel Huber <manuel.h87@gmail.com>
2019-07-29 23:15:15 +0200

    nursing-time: Remove unneeded import

--- app/src/main/java/org/booncode/thebabyapp/ui/ntl/NursingTimeFragment.kt
+++ app/src/main/java/org/booncode/thebabyapp/ui/ntl/NursingTimeFragment.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
